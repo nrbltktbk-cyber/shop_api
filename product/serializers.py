c2deb1f479a23e3f0cf27f17c244ef6f3dc4cd5c
NurbolToktobekov <nrbltktbk-cyber>
2026-04-09 21:10:16 +0600

    hw4

--- product/serializers.py
+++ product/serializers.py
@@ -3,23 +3,59 @@ from .models import Category, Product, Review
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    products_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Category
-        fields = ['id', 'name', 'products_count']
+        fields = ['id', 'name']
 
-    def get_products_count(self, obj):
-        return obj.products.count()
+    # кастомная валидация
+    def validate_name(self, value):
+        if not value:
+            raise serializers.ValidationError("Название не может быть пустым")
+
+        if len(value) < 2:
+            raise serializers.ValidationError("Минимум 2 символа")
+
+        return value
 
 
+# ===== PRODUCT =====
 class ProductSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Product
         fields = ['id', 'title', 'description', 'price', 'category']
 
+    def validate_title(self, value):
+        if not value:
+            raise serializers.ValidationError("Название обязательно")
+        return value
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Цена должна быть больше 0")
+        return value
+
+    def validate(self, data):
+        # общая валидация
+        if len(data.get('description', '')) < 5:
+            raise serializers.ValidationError({
+                "description": "Описание слишком короткое"
+            })
+        return data
 
 class ReviewSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Review
         fields = ['id', 'text', 'stars', 'product']
+
+    def validate_text(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Отзыв слишком короткий")
+        return value
+
+    def validate_stars(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Оценка должна быть от 1 до 5")
+        return value
